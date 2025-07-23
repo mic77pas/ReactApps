4380ba15632c4091404543185b082b070e286ab4
Michael Pasyechnyk <mic77p@gmail.com>
2025-07-22 20:39:16 -0400

    Class components example finalized

--- classy-weather/src/App.js
+++ classy-weather/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Class Components
 // Extending/making components using classes
 
 class Counter extends React.Component {
@@ -7,14 +8,31 @@ class Counter extends React.Component {
     super(props);
 
     this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
   }
 
   render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{date.toDateString()} {this.state.count}</span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
