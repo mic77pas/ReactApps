87ff4cee27c423b819ec231081ddd7dd9e48392b
Michael Pasyechnyk <mic77p@gmail.com>
2025-07-22 20:30:53 -0400

    Class components examples

---
+++ classy-weather/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// Extending/making components using classes
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+  }
+
+  render() {
+    return (
+      <div>
+        <button>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
