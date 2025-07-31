a302b4ed23472891d42e971572bd30e004edf849
Michael Pasyechnyk <mic77p@gmail.com>
2025-07-31 19:23:03 -0400

    Questions added

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -36,7 +36,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
 
@@ -57,7 +57,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]}/>}
       </Main>
     </div>
   );
