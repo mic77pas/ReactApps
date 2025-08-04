7aa772af9ef6e44168fc67b4bd3c53253c7ab661
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-04 18:01:59 -0400

    CitiesContext added

--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -1,17 +1,26 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
-import Spinner from "./Spinner"
-import Message from "./Message"
+import Spinner from "./Spinner";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
-  if(!cities.length) return <Message message='Add your first city by clicking on a city on the map'/>
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
 
-  return <ul className={styles.cityList}>
-    {cities.map(city => (<CityItem city={city} key={city.id} />
-    ))}
-  </ul>;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
