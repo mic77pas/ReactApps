034c435ab9aec431deb66f95491ff74c412f7be1
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-04 18:46:44 -0400

    Leaflet map added

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -10,7 +10,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider, useCities } from "./contexts/CitiesContext";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
   return (
