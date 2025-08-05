25e7b6380937ca84b198e05ae454d8b4b8729744
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-04 23:11:23 -0400

    Refactored using Context API

--- react-quiz/src-no-context/components/DateCounter.js
+++ react-quiz/src-no-context/components/DateCounter.js
@@ -8,21 +8,30 @@ function reducer(state, action) {
   switch (action.type) {
     case "dec":
       return { ...state, count: state.count - state.step };
+
     case "inc":
       return { ...state, count: state.count + state.step };
+
     case "setCount":
       return { ...state, count: action.payload };
+
     case "setStep":
       return { ...state, step: action.payload };
+
     case "reset":
       return initialState;
+
     default:
       throw new Error("Unknown action");
   }
+  // if (action.type === "inc") return state + 1;
+  // if (action.type === "dec") return state - 1;
+  // if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
   const { count, step } = state;
 
   // This mutates the date object.
@@ -31,10 +40,14 @@ function DateCounter() {
 
   const dec = function () {
     dispatch({ type: "dec" });
+    // setCount((count) => count - 1);
+    //setCount((count) => count - step);
   };
 
   const inc = function () {
     dispatch({ type: "inc" });
+    // setCount((count) => count + 1);
+    //setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
@@ -47,6 +60,8 @@ function DateCounter() {
 
   const reset = function () {
     dispatch({ type: "reset" });
+    //setCount(0);
+    //setStep(1);
   };
 
   return (
--- react-quiz/src-no-context/components/Timer.js
+++ react-quiz/src-no-context/components/Timer.js
@@ -1,9 +1,6 @@
 import { useEffect } from "react";
-import { useQuiz } from "../contexts/QuizContext";
 
-function Timer() {
-  const { dispatch, secondsRemaining } = useQuiz();
-
+function Timer({ dispatch, secondsRemaining }) {
   const mins = Math.floor(secondsRemaining / 60);
   const seconds = secondsRemaining % 60;
 
@@ -21,7 +18,8 @@ function Timer() {
   return (
     <div className="timer">
       {mins < 10 && "0"}
-      {mins}:{seconds < 10 && "0"}
+      {mins}:
+      {seconds < 10 && "0"}
       {seconds}
     </div>
   );
