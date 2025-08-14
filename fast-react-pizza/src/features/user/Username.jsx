a432f5e0728858d806ea17b4868bb9a1b492ec3e
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-14 15:17:33 -0400

    Flex added to homepage

--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Michael</div>;
+  return <div className="hidden text-sm font-semibold md:block">Michael</div>;
 }
 
 export default Username;
