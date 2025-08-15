610bf30f27f2e0afc1f9d19ff67ad95665d63cba
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-15 11:25:12 -0400

    Custom tailwind extended

--- fast-react-pizza/tailwind.config.js
+++ fast-react-pizza/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+      // fontSize: {
+      //   huge: ['80rem', { lineHeight: '1'}]
+      // }
+      // colors: {
+      //   pizza: "#12345",
+      // },
+    },
   },
   plugins: [],
 };
