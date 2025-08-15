9d67d969f438d86683bdceeff1a665e60ddb8347
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-15 13:32:25 -0400

    Cart page updated

--- fast-react-pizza/src/features/user/userSlice.js
+++ fast-react-pizza/src/features/user/userSlice.js
@@ -38,4 +38,6 @@ const userSlice = createSlice({
 
 export const { updateName } = userSlice.actions;
 
+export const getUsername = (state) => state.user.username;
+
 export default userSlice.reducer;
