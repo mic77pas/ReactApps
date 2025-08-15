d811abdf8421ba3c44b7df1f46c7954a136cea73
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-15 11:44:23 -0400

    Menu styling added

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+function Button({ children, disabled, to, type }) {
+  // const className =
+  //   "bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
 
-  if (to) return <Link to={to} className={className}>{children}</Link>;
+  const base = "bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+  }
+
+  if (to) return <Link to={to} className={styles[type]}>{children}</Link>;
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
