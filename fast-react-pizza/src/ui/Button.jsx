e4fb6104018d751c56d874fa2663536c0305fcf7
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-15 12:06:11 -0400

    Order form styling added

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -5,13 +5,13 @@ function Button({ children, disabled, to, type }) {
   //   "bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
 
   const base =
-    "bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    "text-sm bg-yellow-400 uppercase font-semibold tracking-wide text-stone-800 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
     secondary:
-      "border-2 border-stone-300 uppercase font-semibold tracking-wide text-stone-400 inline-block rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors focus:text-stone-800 duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
+      "text-sm border-2 border-stone-300 uppercase font-semibold tracking-wide text-stone-400 inline-block rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors focus:text-stone-800 duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
 
   if (to)
