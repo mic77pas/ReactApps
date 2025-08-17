40d3d3a7743b558fd210085263925e5586613a57
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-17 15:49:51 -0400

    Uploading images to cabin form implemented

--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vuvwxuvjkyzcxsmfftdi.supabase.co";
+export const supabaseUrl = "https://vuvwxuvjkyzcxsmfftdi.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1dnd4dXZqa3l6Y3hzbWZmdGRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzNzQxNDIsImV4cCI6MjA3MDk1MDE0Mn0.h1QTZZXsyZa1TqYKvSnBzRghTndRKbg5nSED03fKigI";
 const supabase = createClient(supabaseUrl, supabaseKey);
