0adacddd08e6ef5e0b525d0866feb96b52a11514
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-20 18:36:16 -0400

    Project folders organized

--- the-wild-oasis-website/app/page.js
+++ the-wild-oasis-website/app/page.js
@@ -1,14 +1,12 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
+import Navigation from "./_components/Navigation";
 
 export default function Page() {
   return (
     <div>
-
       <h1>The Wild Oasis. Welcome to Paradise.</h1>
 
-      <Link href='/cabins'>Explore luxury cabins</Link>
-
+      <Link href="/cabins">Explore luxury cabins</Link>
     </div>
   );
 }
