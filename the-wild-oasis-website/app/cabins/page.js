664fd01778abc53ad2616ad64ce4fc274fee0c66
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-20 18:52:18 -0400

    Metadata and favicon added

--- the-wild-oasis-website/app/cabins/page.js
+++ the-wild-oasis-website/app/cabins/page.js
@@ -1,7 +1,10 @@
 import Counter from "@/app/_components/Counter";
 
-export default function Page() {
+export const metadata = {
+  title: "Cabins",
+};
 
+export default function Page() {
   return (
     <div>
       <h1>Cabins page</h1>
